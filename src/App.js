41c4f17e2d503acf5a894195a4516d2c33962525
Bruno <bruno.umbelino0@gmail.com>
2021-02-17 12:01:37 -0300

    implementing SASS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import "./App.css";
+import { Provider } from "react-redux";
 import { Main } from "./components/MainComponent";
+import { store } from "./redux/store";
 
 function App() {
-  return <Main />;
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 }
 
 export default App;
